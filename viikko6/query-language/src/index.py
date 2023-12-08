8bb2eb91de24ae4f4442226c489c5ef34e1717ed
liisa <liisa-m@outlook.com>
2023-12-08 15:28:49 +0200

    vk6 t3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -12,17 +12,31 @@ def main():
         HasAtLeast(5, "goals"),
         HasAtLeast(20, "assists"),
         PlaysIn("PHI")
-    )'''
+    )
 
-    '''
+    
     matcher = And(
         Not(HasAtLeast(2, "goals")),
         PlaysIn("NYR")
-    )'''
+    )
 
     matcher = And(
         HasFewerThan(2, "goals"),
         PlaysIn("NYR")
+    )
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )'''
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -10,6 +10,16 @@ class And:
         return True
 
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+
 class Not:
     def __init__(self, *matchers):
         self._matchers = matchers
